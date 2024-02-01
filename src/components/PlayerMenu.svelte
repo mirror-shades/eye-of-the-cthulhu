9b8208bb4a7718c890dc93400123af2f2bbb6830
mirror-shades <44949816+mirror-shades@users.noreply.github.com>
2024-01-31 22:00:32 -0800

    add icon

--- src/components/PlayerMenu.svelte
+++ src/components/PlayerMenu.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import PlayerPage from "./playerPage.svelte";
+  //@ts-ignore variable generated from a js file
   import { playerList } from "../assets/buildPlayerList";
   import cliff from "../assets/cliff.png";
   import max from "../assets/max.png";
@@ -10,32 +11,37 @@
   let dummy;
   let page = "cliff";
   let description = "Cliff Bucco, Police Officer";
+  //@ts-ignore variable generated from a js file
   let playerCharacter = playerList[0];
   const changePage = (name: string) => {
     page = name;
     if (name === "cliff") {
       description = "Cliff Bucco, Police Officer";
+      //@ts-ignore variable generated from a js file
       playerCharacter = playerList[0];
       picture = cliff;
     }
     if (name === "emily") {
       description = "Emily Rosewell, Typist";
+      //@ts-ignore variable generated from a js file
       playerCharacter = playerList[1];
-      console.log(playerList[1]);
       picture = emily;
     }
     if (name === "max") {
       description = "Maximillian Hirst, Parapsychologist";
+      //@ts-ignore variable generated from a js file
       playerCharacter = playerList[2];
       picture = max;
     }
     if (name === "mcgee") {
       description = "Shitter Mc'Gee, Professor of Geology";
+      //@ts-ignore variable generated from a js file
       playerCharacter = playerList[3];
       picture = mcgee;
     }
     if (name === "melissa") {
       description = "Melissa Devries, Biologist";
+      //@ts-ignore variable generated from a js file
       playerCharacter = playerList[4];
       picture = melissa;
     }
